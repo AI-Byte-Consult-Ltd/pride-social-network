baf3ab53a7d9b544f684b79ae2a87ae29d5d45d2
Alexander Lunin <info@aibyteconsult.com>
2025-10-17 20:03:35 +0000

    deploy: update build

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // 👈 критично важно
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
